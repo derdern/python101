69863d51d5bb7a3b76cecebfe5e426cc3ceeb61a
derdern <tderdern@gmail.com>
2023-08-11 10:47:24 +0700

    mod3

--- main1.py
+++ main1.py
@@ -35,4 +35,4 @@ str_stamp = da.text
 datetime_obj = datetime.strptime(str_stamp, '%d/%m/%Y').date()
 date = str(datetime_obj)
 
-print("this is data " + date,g1b,g1s,g2b,g2s)
+print("this is data of gold price" + date,g1b,g1s,g2b,g2s)
